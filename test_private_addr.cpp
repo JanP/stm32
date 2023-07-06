55f18e5e980696e9758eb6ae595f80454e1b2ede
Jan Paesmans <jan.paesmans@gmail.com>
2023-07-06 22:23:18 +0200

    Update code and add variant

--- test_private_addr.cpp
+++ test_private_addr.cpp
@@ -2,20 +2,21 @@
 #include <cstring>
 #include <type_traits>
 
-template <volatile uint32_t * ADDR>
 class Register {
     public:
-        Register() {}
+        Register(volatile uint32_t* addr) : m_addr(addr) {}
 
-        inline uint32_t read() { return *ADDR; }
-        inline void write(uint32_t value) { *ADDR = value; }
+        inline uint32_t read() { return *m_addr; }
+        inline void write(uint32_t value) { *m_addr = value; }
 
-        template<class CALLABLE, std::enable_if_t<std::is_invocable_v<CALLABLE, uint32_t>, bool> = false>
-        inline void modify(CALLABLE callable) { *ADDR = callable(*ADDR); }
+        template<class FuncPtr, std::enable_if_t<std::is_invocable_v<FuncPtr, uint32_t>, bool> = false>
+        inline void modify(FuncPtr mod_functor) { *m_addr = mod_functor(*m_addr); }
+
+    private:
+        volatile uint32_t* const m_addr;
 };
 
-template <volatile uint32_t * ADDR>
-class StatusRegister : Register<ADDR> {
+class StatusRegister : Register {
     public:
         enum class State {
             Active,
@@ -31,8 +32,8 @@ class StatusRegister : Register<ADDR> {
                 inline bool busy() const { return m_bits.busy; }
                 inline State state() const { return static_cast<State>(m_bits.state); }
                 inline bool frame_error() const { return m_bits.frame_error; }
-                inline bool overflow_error() const { return m_bits.ovfl_error; }
-                inline void clear_overflow_error() { m_bits.ovfl_error = 1; }
+                inline bool overflow_error() const { return m_bits.overflow_error; }
+                inline void clear_overflow_error() { m_bits.overflow_error = 1; }
                 inline void clear_frame_error() { m_bits.frame_error = 1; }
 
                 inline uint32_t reg_value() const {
@@ -46,25 +47,25 @@ class StatusRegister : Register<ADDR> {
                     uint32_t busy : 1;
                     uint32_t state : 2;
                     uint32_t : 3;
-                    uint32_t ovfl_error : 1;
+                    uint32_t overflow_error : 1;
                     uint32_t frame_error : 1;
                     uint32_t : 24;
                 } m_bits;
         };
 
-        StatusRegister() : Register<ADDR>() {}
-        Fields read() { return Fields{Register<ADDR>::read()}; }
+        StatusRegister(volatile uint32_t * addr) : Register(addr) {}
+        Fields read() { return Fields{Register::read()}; }
 
         template <class CALLABLE, std::enable_if_t<std::is_invocable_v<CALLABLE, Fields&>, bool> = false>
         void write(CALLABLE callable) {
             Fields f;
             callable(f);
-            Register<ADDR>::write(f.reg_value());
+            Register::write(f.reg_value());
         }
 
         template <class CALLABLE, std::enable_if_t<std::is_invocable_v<CALLABLE, const Fields&, Fields&>, bool> = false>
         void modify(CALLABLE callable) {
-            Register<ADDR>::modify([=](uint32_t r_val) {
+            Register::modify([=](uint32_t r_val) {
                 Fields r{r_val};
                 Fields w;
                 callable(r, w);
@@ -76,7 +77,7 @@ class StatusRegister : Register<ADDR> {
 static uint32_t status_reg_mem = 0;
 
 void clear_frame_error() {
-    StatusRegister<&status_reg_mem> status_reg;
+    StatusRegister status_reg(&status_reg_mem);
     status_reg.modify([=](const auto &r, auto &w) {
         if (r.frame_error()) {
             w.clear_frame_error();
@@ -85,7 +86,7 @@ void clear_frame_error() {
 }
 
 void clear_overflow_error() {
-    StatusRegister<&status_reg_mem> status_reg;
+    StatusRegister status_reg(&status_reg_mem);
     status_reg.modify([=](const auto &r, auto &w) {
         if (r.overflow_error()) {
             w.clear_overflow_error();
@@ -94,11 +95,11 @@ void clear_overflow_error() {
 }
 
 bool is_busy() {
-    StatusRegister<&status_reg_mem> status_reg;
+    StatusRegister status_reg(&status_reg_mem);
     return status_reg.read().busy();
 }
 
 auto get_state() {
-    StatusRegister<&status_reg_mem> status_reg;
+    StatusRegister status_reg(&status_reg_mem);
     return status_reg.read().state();
 }
